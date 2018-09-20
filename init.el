dbdfe0054ed8797de76b3c9aa59110b24557120a
CannedCans <CannedCans@users.noreply.github.com>
2018-09-20 15:51:51 -0400

    Added comments

--- init.el
+++ init.el
@@ -1,14 +1,21 @@
 (package-initialize)
 
-(global-linum-mode 60)
-(display-time-mode 60)
-(display-battery-mode 1)
+(global-linum-mode 60)   ;; Puts line numbers on the left of every buffer
+(display-time-mode 60)   ;; Puts a time display on the bottom
+(display-battery-mode 1) ;; Shows the battery at the bottom
 
+;; Sets the shell for M-x shell as bash, Emacs doesn't play well
+;; with my shell of choice, fish
 (setq-default explicit-shell-file-name "/bin/bash")
 
+;; Make M-up arrow, down arrow act the same as C-up arrow, down arrow
+;; Easier to press
 (global-set-key (kbd "M-<up>") 'backward-paragraph)
 (global-set-key (kbd "M-<down>") 'forward-paragraph)
 
+;; Makes any links go through EWW instead of your normal browser
+;; This is great for RSS feeds through Elfeed
 (setq browse-url-browser-function 'eww-browse-url)
 
+;; This turns on company-mode in all buffers
 (add-hook 'after-init-hook 'global-company-mode)
