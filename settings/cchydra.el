bd9f2211189defea3c9f28479307ddc9c753309d
CannedCans <CannedCans@users.noreply.github.com>
2018-09-29 20:11:48 -0400

    Basic buffer manipulation Hydra

--- settings/cchydra.el
+++ settings/cchydra.el
@@ -1,2 +1,14 @@
 ;; For Hydra related things
 
+(defhydra hydra-buffer-manipulation (global-map "<f2>")
+  "Manipulate buffers easily"
+  ("u" upcase-word)
+  ("l" downcase-word)
+  ("t" transpose-words)
+
+  ;; NAVIGATION so that we don't break out of the Hydra
+  ("<right>" right-word)
+  ("<left>" left-word)
+  ("<up>" previous-line)
+  ("<down>" next-line)
+)
