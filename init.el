e43068879352e74f3daadb8ea901f81a5fdab4ba
CannedCans <CannedCans@users.noreply.github.com>
2019-01-10 20:03:31 -0500

    Added org keybindings

--- init.el
+++ init.el
@@ -30,6 +30,10 @@
 (require 'org)
 (setq org-directory "~/org-directory")
 
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+
 ;; Add our local MELPA (if available)
 (cc-load-file "localmelpa/cclocalmelpa.el")
 (package-initialize)
