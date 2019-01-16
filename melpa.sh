e9a74b18a0e268359c6446f47f3a2d3ebb36fe51
CannedCans <CannedCans@users.noreply.github.com>
2019-01-16 13:00:15 -0500

    Added magit's dependencies

--- melpa.sh
+++ melpa.sh
@@ -27,6 +27,7 @@ make recipes/haskell-mode
 
 # Packages from the Debian migration are after here
 make recipes/company
+make recipes/async; make recipes/dash; make recipes/git-commit; make recipes/magit-popup; make recipes/with-editor # The dependencies of magit
 make recipes/magit
 
 make json
