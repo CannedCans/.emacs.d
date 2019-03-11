0af3078f451f5f9d30a4050fcafb70ca86c3fbe4
CannedCans <CannedCans@users.noreply.github.com>
2019-03-11 18:34:46 -0400

    Did a temporary fix for magit-commit -> magit-commit-create

--- init.el
+++ init.el
@@ -105,13 +105,14 @@
 ;; Magit keybindings
 (if (package-installed-p 'magit)
     (progn
-      (global-set-key (kbd "C-c M-c") 'magit-commit)
+      ;;(global-set-key (kbd "C-c M-c") 'magit-commit)
+      (global-set-key (kbd "C-c M-c") 'magit-commit-create)
       (global-set-key (kbd "C-c M-s") 'magit-stage-file)
       (global-set-key (kbd "C-c M-p") 'magit-push-to-remote)
       (message "CC - Magit enabled")
       )
-    (message "CC - Magit not installed")
-)
+  (message "CC - Magit not installed")
+  )
 
 ;; Elfeed feeds
 (if (package-installed-p 'elfeed)
@@ -187,3 +188,20 @@
     (sml/setup)
     )
   )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(package-selected-packages
+   (quote
+    (yasnippet projectile markdown-mode iedit git-timemachine flx-ido elfeed anzu ws-butler solarized-theme smart-mode-line org-journal org-brain multiple-cursors mediawiki magit-popup magit lv js2-mode hydra helm-org-rifle flycheck-pos-tip flycheck-irony flycheck-haskell company-math chess aggressive-indent))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
